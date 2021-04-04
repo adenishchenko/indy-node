f7066dfc5f71f3d079758dd1f5d79ef0ab527c41
anton.denishchenko <anton.denishchenko@evernym.com>
2021-04-04 05:31:57 +0300

    Fix tests requires
    Signed-off-by: anton.denishchenko <anton.denishchenko@evernym.com>

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ with open(metadata['__file__'], 'r') as f:
 
 BASE_DIR = os.path.join(os.path.expanduser("~"), ".indy")
 
-tests_require = ['attrs==19.1.0', 'pytest==3.3.1', 'pytest-xdist==1.22.1', 'pytest-forked==0.2',
-                 'python3-indy==1.15.0-dev-1618', 'pytest-asyncio==0.8.0']
+tests_require = ['attrs>=20.3.0', 'pytest>=6.2.2', 'pytest-xdist>=2.2.1', 'pytest-forked>=1.3.0',
+                 'python3-indy==1.15.0-dev-1618', 'pytest-asyncio==0.14.0']
 
 setup(
     name=metadata['__title__'],
